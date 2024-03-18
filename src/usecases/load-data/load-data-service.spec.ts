9c928cf9bcdabc5e2a576d8b2d6a94382e1ceb56
De Paula <bdepaula@la.jnj.com@WLLNXPF41LSDR.wks.jnj.com>
2024-03-18 19:54:15 -0300

    Keep calm and commit

--- src/usecases/load-data/load-data-service.spec.ts
+++ src/usecases/load-data/load-data-service.spec.ts
@@ -37,5 +37,6 @@ describe('Test Suite for load-data-service.spec', () => {
     const { sut, repository } = makeSut()
     const result = await sut.load()
     expect(result).toEqual(repository.result)
+    console.log('dsd')
   })
 })
